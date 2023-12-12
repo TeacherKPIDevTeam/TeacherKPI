cdbf1b4568649c1d65d05a93b9c50827ea95876d
DelecD <NULL>
2023-12-12 15:56:36 +0300

    Добавлена работа с БД, извлечение User из БД по API

---
+++ database/dbconnection.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//var db *sql.DB
+
+func RequestQuery(request string, args ...any) *sql.Rows {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/teacher_kpi")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	result, err := db.Query(request, args...)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
+
+func RequestNonQuery(request string, args ...any) {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/teacher_kpi")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(request, args)
+
+	if err != nil {
+		panic(err)
+	}
+}

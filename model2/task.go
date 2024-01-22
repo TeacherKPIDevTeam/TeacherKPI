c3a8877f84ede6ea080df1f882d2098f4e252fc9
DelecD <NULL>
2024-01-22 16:57:49 +0300

    Merge pull request #5 from TeacherKPIDevTeam/dmitriy_usermodel Repositories, changes in database module, etc

---
+++ model2/task.go
@@ -0,0 +1,20 @@
+package model2
+
+import "database/sql"
+
+type Task struct {
+	Id     int `json:"id"`
+	UserId int `json:"userId"`
+	//Индекс типа задач
+	TypeId    int    `json:"type"`
+	TypeName  string `json:"typeName"`
+	StagesIds []int  `json:"stagesIds"`
+}
+
+func (task *Task) ScanFromRows(rows *sql.Rows) error {
+	ret := new(Task)
+	if err := rows.Scan(&ret.Id, &ret.UserId, &ret.TypeId); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ repos2/task.go
@@ -0,0 +1,114 @@
+package repos2
+
+import (
+	"TeacherKPI/database"
+	"TeacherKPI/model"
+)
+
+type TaskRepo struct {
+	Db *database.DbConnection
+}
+
+func (repo TaskRepo) Create(task *model.Task) error {
+	result, err := repo.Db.RequestNonQuery("INSERT INTO tasks VALUES(NULL, ?, ?)", task.UserId, task.TypeId)
+	if err != nil {
+		return err
+	}
+	id, _ := result.LastInsertId()
+	task.Id = int(id)
+	return nil
+}
+
+func (repo TaskRepo) GetById(id int) (*model.Task, error) {
+	response, err := repo.Db.RequestQuery("SELECT * FROM tasks WHERE id=?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	response.Next()
+	defer response.Close()
+
+	task := new(model.Task)
+	if err := response.Scan(&task.Id, &task.UserId, &task.TypeId); err != nil {
+		return nil, err
+	}
+	return task, nil
+}
+
+func (repo TaskRepo) GetTaskStages(task *model.Task) ([]*model.Stage, error) {
+	response, err := repo.Db.RequestQuery("SELECT * FROM tasks WHERE user_id=?", task.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	response.Next()
+	defer response.Close()
+
+	ret := []*model.Stage{}
+
+	for {
+		if !response.Next() {
+			break
+		}
+
+		stage := new(model.Stage)
+		if err := response.Scan(&stage.Id); err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, stage)
+	}
+
+	return ret, nil
+}
+
+func (repo TaskRepo) GetTaskStagesIds(taskId int) ([]int, error) {
+	response, err := repo.Db.RequestQuery("SELECT * FROM stage WHERE user_id=?", taskId)
+	if err != nil {
+		return nil, err
+	}
+
+	response.Next()
+	defer response.Close()
+
+	ret := []int{}
+	var tempId int
+	for {
+		if !response.Next() {
+			break
+		}
+
+		if err := response.Scan(&tempId); err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, tempId)
+	}
+	return ret, nil
+}
+
+func (repo TaskRepo) AddStage(task *model.Task, stage *model.Stage) {
+	for _, v := range task.StagesIds {
+		if v == task.Id {
+			return
+		}
+	}
+	task.StagesIds = append(task.StagesIds, stage.Id)
+	repo.Update(task)
+}
+
+func (repo TaskRepo) Update(task *model.Task) error {
+	_, err := repo.Db.RequestNonQuery("UPDATE tasks SET user_id=?, type_id=? WHERE id=?",
+		task.UserId,
+		task.TypeId,
+		task.Id)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func (repo TaskRepo) Delete(task *model.Task) error {
+	//TODO
+	return nil
+}
